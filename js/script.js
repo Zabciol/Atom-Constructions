97bc484093d83f674b80b6c7cf1d7ead44f51789
Zabciol <janek.zaba05@gmail.com>
2022-07-08 17:41:10 +0200

    poprawki

---
+++ js/script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function(){
+const nav = document.querySelector('.navbar');
+const button = document.querySelector('.fa-bars');
+const showNav = document.querySelector('.navbar-collapse');
+const allNavItems = document.querySelectorAll('.nav-link');
+const logo = document.querySelector('.navbar-brand')
+
+function addShadow() {
+
+    if(window.innerWidth< 993)
+    {
+        if (window.scrollY >= 10)
+        {
+            nav.classList.add('shadow-bg')
+        }else
+        {
+            nav.classList.remove('shadow-bg')
+        }
+    }else{
+        if (window.scrollY >= 300)
+        {
+            nav.classList.add('shadow-bg')
+        }else
+        {
+            nav.classList.remove('shadow-bg')
+        }
+
+
+    }
+}
+function navButton() {
+    if(window.screen.availHeight< 840)
+    {
+        nav.classList.add('shadow-bg')
+    }
+}
+function closeNav(){
+    showNav.classList.remove('show')
+}
+
+allNavItems.forEach(item => item.addEventListener('click', closeNav))
+window.addEventListener('scroll',addShadow)
+button.addEventListener('click',navButton)
+logo.addEventListener('click',closeNav)
+})
