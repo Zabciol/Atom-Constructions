d35f17f75c2f22f7b55d1f6b97c625b3c5ea0530
Zabciol <janek.zaba05@gmail.com>
2022-08-05 14:32:36 +0200

    poprawki

--- js/script.js
+++ js/script.js
@@ -1,37 +1,31 @@
-document.addEventListener('DOMContentLoaded', function(){
-const nav = document.querySelector('.navbar');
-const button = document.querySelector('.fa-bars');
-const showNav = document.querySelector('.navbar-collapse');
-const allNavItems = document.querySelectorAll('.nav-link');
-const logo = document.querySelector('.navbar-brand')
+document.addEventListener('DOMContentLoaded', function () {
+	const nav = document.querySelector('.navbar');
+	const button = document.querySelector('.fa-bars');
+	const showNav = document.querySelector('.navbar-collapse');
+	const allNavItems = document.querySelectorAll('.nav-link');
+	const logo = document.querySelector('.navbar-brand');
+	const contact = document.querySelector('.contact');
 
-function addShadow() {
+	function addShadow() {
+		if (window.scrollY >= 300) {
+			nav.classList.add('shadow-bg');
+		} else {
+			nav.classList.remove('shadow-bg');
+		}
+	}
 
- 
-        if (window.scrollY >= 300)
-        {
-            nav.classList.add('shadow-bg')
-        }else
-        {
-            nav.classList.remove('shadow-bg')
-        }
+	function navButton() {
+		if (window.screen.availHeight < 750 || window.scrollY >= 100) {
+			nav.classList.add('shadow-bg');
+		}
+	}
 
-}
+	function closeNav() {
+		showNav.classList.remove('show');
+	}
 
-function navButton() {
-    if(window.screen.availHeight<750 || window.scrollY >= 100 )
-    {
-        nav.classList.add('shadow-bg')
-    }
-}
-
-function closeNav(){
-    showNav.classList.remove('show')
-}
-
-allNavItems.forEach(item => item.addEventListener('click', closeNav))
-window.addEventListener('scroll',addShadow)
-button.addEventListener('click',navButton)
-logo.addEventListener('click',closeNav)
-
-})
+	allNavItems.forEach((item) => item.addEventListener('click', closeNav));
+	window.addEventListener('scroll', addShadow);
+	button.addEventListener('click', navButton);
+	logo.addEventListener('click', closeNav);
+});
